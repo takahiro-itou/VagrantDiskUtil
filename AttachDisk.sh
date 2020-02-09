4097fc3cfbd99e5f2738d82250c6304002cefdda
Takahiro Itou <takahiro.i.180630@gmail.com>
2020-02-09 13:06:15 +0900

    不具合修正

--- AttachDisk.sh
+++ AttachDisk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -ue
+#!/bin/bash  -xue
 
 if [ $# -lt 2 ]; then
     echo "Usage: $0 [VM name] [DiskImage]"  1>&2
@@ -10,19 +10,20 @@ vmName=$1
 diskImage=$2
 
 # 必要なパラメータを取得
-vmUUID=$(${VBoxManage}  list  vms | fgrep ${vmName} | awk '{ print $2; }')
+vmUUID=$(${VBoxManage}  list  vms | fgrep ${vmName} \
+    | awk '{ print $2; }' | sed -r "s/^\{(.*)\}\r?$/\1/")
 echo  "Target UUID = ${vmUUID}"
 
 # 既存の仮想ディスクをデタッチ
 ${VBoxManage}  storageattach ${vmUUID}  \
-    --storagectl  "SCSI Controller"     \
+    --storagectl  "SCSI"                \
     --port    0                         \
     --type    hdd                       \
     --medium  emptydrive
 
 # 新しい仮想ディクスをアタッチ
 ${VBoxManage}  storageattach ${vmUUID}  \
-    --storagectl  "SCSI Controller"     \
+    --storagectl  "SCSI"     \
     --device  0                         \
     --port    0                         \
     --type    hdd                       \
