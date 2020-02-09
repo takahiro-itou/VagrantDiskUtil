769c6a2cc1a458375920ba032ac7c3f8674d0ac4
Takahiro Itou <takahiro.i.180630@gmail.com>
2020-02-09 14:53:00 +0900

    不具合修正

--- Compress.sh
+++ Compress.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -xue
+#!/bin/bash  -xu
 
 if [ $# -lt 1 ]; then
     echo  "Usage: $0 (DiskImage) [WorkFile]"  1>&2
@@ -10,16 +10,27 @@ diskFile=$1
 workFile=$2
 
 # ファイルフォーマットを変換する。
-${VBoxManage}  clonehe              \
-    old.${diskFile}  ${workFile}    \
-     --format vdi
+rm -f  ${workFile}
+
+stdout=$(${VBoxManage}  clonehd              \
+    ${diskFile}  ${workFile}        \
+     --format vdi)
+echo  "${stdout}"
+workUUID=$(echo -n ${stdout} | tr -d '\r' | sed -r "s/^.*\. UUID: (.*)$/\1/")
 
 # vdi ファイルを圧縮する。
 ${VBoxManage}  modifyhd             \
     ${workFile}  --compact
 
+# 元のファイルをバックアップする。
+# mv  -v  ${diskFile}  ${diskFile}.old
+
 # 元の vmdk 形式に変換する。
+rm -f  ${diskFile}.new
 ${VBoxManage}  clonehd  \
-    ${workFile}  new.${diskFile}    \
+    ${workFile}  ${diskFile}.new    \
     --format vmdk                   \
     --variant Split2G
+
+# 作業イメージをリストから除外する。
+${VBoxManage}  closemedium disk  ${workUUID}
